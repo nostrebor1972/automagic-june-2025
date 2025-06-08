761913fe59182ae1267608fbd5951e1a03f0087c
mkol5222 <mkoldov@gmail.com>
2025-06-08 15:31:54 +0000

    .

--- policy/terraform/host.tf
+++ policy/terraform/host.tf
@@ -1,4 +1,7 @@
 resource "checkpoint_management_host" "example" {
-  name = "New Host 1"
+  name = "tf-host"
   ipv4_address = "192.0.2.1"
+  color = "blue"
+  tags = ["tag1", "tag2", "madeByTf"]
+  comments = "This is a new host"
 }
