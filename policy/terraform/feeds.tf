e02a4b0688ed9e69440a691fa2dca05244635f41
Martin Koldovsky <mkoldov@gmail.com>
2025-07-22 12:41:42 +0000

    .

--- policy/terraform/feeds.tf
+++ policy/terraform/feeds.tf
@@ -20,3 +20,38 @@ resource "checkpoint_management_network_feed" "quiccloud" {
   feed_type       = "IP Address"
   update_interval = 60
 }
+
+# feed_dev_airflow
+
+resource "checkpoint_management_network_feed" "f1" {
+  name     = "feed_dev_airflow"
+  feed_url = "https://feed-serv.deno.dev/ip"
+  #   username = "feed_username"
+  #   password = "feed_password"
+  feed_format     = "JSON"
+  feed_type       = "IP Address"
+  update_interval = 60
+  json_query      = ".[]|.ip"
+}
+
+resource "checkpoint_management_network_feed" "f2" {
+  name     = "feed_dev_cicd-jenkins"
+  feed_url = "https://feed-serv.deno.dev/ip"
+  #   username = "feed_username"
+  #   password = "feed_password"
+  feed_format     = "JSON"
+  feed_type       = "IP Address"
+  update_interval = 60
+  json_query      = ".[]|.ip"
+}
+
+resource "checkpoint_management_network_feed" "f3" {
+  name     = "feed_dev_cicd-formule1"
+  feed_url = "https://feed-serv.deno.dev/ip"
+  #   username = "feed_username"
+  #   password = "feed_password"
+  feed_format     = "JSON"
+  feed_type       = "IP Address"
+  update_interval = 60
+  json_query      = ".[]|.ip"
+}
